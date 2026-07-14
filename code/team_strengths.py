df0fe088b5f8ecfae22af9f79ca22ecabf1f6be1
Jasper Mclennan <jasper.mclennan@objentis.com>
2026-07-14 12:09:07 +0200

    team id setup

--- code/team_strengths.py
+++ code/team_strengths.py
@@ -1,3 +1,20 @@
 import fetcher
 
 print(fetcher.strong_correlation_df)
+
+
+
+team_ids = {
+
+    "0" : "EDM",
+    "1" : "ITO",
+    "2" : "VAL",
+    "3" : "HFX",
+    "4" : "CAV",
+    "5" : "PAC",
+    "6" : "ATO",
+    "7" : " FOR", 
+    
+    }
+
+
