c8713297d1c29eb96592d5f383bd1e628b69830e
Jasper Mclennan <jasper.mclennan@objentis.com>
2026-07-14 11:10:25 +0200

    new ingestion strategy to import all cpl seasons concurrenlty

--- code/fetcher.py
+++ code/fetcher.py
@@ -6,55 +6,60 @@ import os
 
 base_url = "https://api-sdp.cplsoccer.com/v1/cpl/football/seasons/cpl::Football_Season::c479ab0916a24c3390f1ce2c021ace54/stats/teams?locale=en-US"
 
-# season_ids = {
-#
-#     "2021" : "2f07c39671b84933ad7bb1e1958a7427",
-#     "2022" : "046f0ab31ba641c7b7bf27eb0dda4b9d",
-#     "2023" : "fc0855108c9044218a84fc5d2bee0000",
-#     "2024" : "6fb9e6fae4f24ce9bf4fa3172616a762",
-#     "2025" : "fd43e1d61dfe4396a7356bc432de0007",
-#     "2026" : "c479ab0916a24c3390f1ce2c021ace54",
-# }
+season_ids = {
 
-response = requests.get(base_url)
-
-#check to make sure the request was successful
-if response.status_code == 200:
-    print("Request was successful")
-else:
-    print(f"Failed to retrieve data: {response.status_code}")
+    "2021" : "2f07c39671b84933ad7bb1e1958a7427",
+    "2022" : "046f0ab31ba641c7b7bf27eb0dda4b9d",
+    "2023" : "fc0855108c9044218a84fc5d2bee0000",
+    "2024" : "6fb9e6fae4f24ce9bf4fa3172616a762",
+    "2025" : "fd43e1d61dfe4396a7356bc432de0007",
+    "2026" : "c479ab0916a24c3390f1ce2c021ace54",
+}
 
 
-data = response.json()
+for season in season_ids:
+    season_id = season_ids[season]
+    base_url = f"https://api-sdp.cplsoccer.com/v1/cpl/football/seasons/cpl::Football_Season::{season_id}/stats/teams?locale=en-US"
 
-rows = []
-for team in data["teams"]:
-    row = {
-       
-        "acronymName": team["acronymName"],
-         "shortName": team["shortName"],
-    }
-    for stat in team["stats"]:
-        row[stat["statsId"]] = stat["statsValue"]
-    rows.append(row)
+    response = requests.get(base_url)
 
-teams_data = pd.DataFrame(rows)
+    #check to make sure the request was successful
+    if response.status_code == 200:
+        print("Request was successful for year:", season)
+    else:
+        print(f"Failed to retrieve data for year {season}: {response.status_code}")
 
 
-average_df =  teams_data.mean(numeric_only=True, axis=0)
-stddev_df =  teams_data.std(numeric_only=True, axis=0)
-zscore_df = (teams_data - average_df) / stddev_df
+    data = response.json()
 
-correlation_df = teams_data.corrwith(teams_data['total-points'],numeric_only=True)
-strong_correlation_df = correlation_df[correlation_df.abs() > 0.7]
+    rows = []
+    for team in data["teams"]:
+        row = {
+        
+            "acronymName": team["acronymName"],
+            "shortName": team["shortName"],
+        }
+        for stat in team["stats"]:
+            row[stat["statsId"]] = stat["statsValue"]
+        rows.append(row)
+
+    teams_data = pd.DataFrame(rows)
 
 
-print(teams_data)
+    average_df =  teams_data.mean(numeric_only=True, axis=0)
+    stddev_df =  teams_data.std(numeric_only=True, axis=0)
+    zscore_df = (teams_data - average_df) / stddev_df
 
-print("__________________________________________________________________________________________________________")
+    correlation_df = teams_data.corrwith(teams_data['total-points'],numeric_only=True)
+    strong_correlation_df = correlation_df[correlation_df.abs() > 0.7]
 
-print(strong_correlation_df)
+
+    print(teams_data)
+
+    print("__________________________________________________________________________________________________________")
+
+    print(strong_correlation_df)
 
 
-# os.makedirs("data/team", exist_ok=True)
-# teams_data.to_csv("data/team/2026_team_data.csv", index=False, sep=';')
+    os.makedirs("data/team", exist_ok=True)
+    teams_data.to_csv(f"data/team/{season}_team_data.csv", index=False, sep=';')
